f33b45b6b0e04fbb4becc452c6a3ae1b2559dca8
Alexander Chacon <fresnedaalexander@gmail.com>
2024-05-20 01:12:10 -0500

    Desarrollo 1 clase pokemon

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -1,5 +1,37 @@
 package simulador.pokemon;
 
-public class Pokemon {
-    // TODO: Aquí va tu código
+public abstract class Pokemon {
+    private String nombre;
+    // El nombre del Pokémon.
+    
+    private double salud;
+    //Puntos de vida del Pokémon.
+    
+    private double puntosDeAtaque;
+    // La potencia base de los ataques del Pokémon
+    
+    private String tipo;
+    //El tipo elemental del Pokémon, basado en la enumeración TipoPokemon.
+    
+    private String estado;
+    //El tipo elemental del Pokémon, basado en la enumeración TipoPokemon.
+    
+    
+    public Pokemon(String nombre, double salud, double puntosDeAtaque, String tipo, String estado) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.puntosDeAtaque = puntosDeAtaque;
+        this.tipo = tipo;
+        this.estado = estado;
+    }
+    
+    public abstract void atacar();
+    //Realiza un ataque al oponente, teniendo en cuenta las ventajas y desventajas de tipo.
+ 
+    public abstract void recibirDaño(int daño);
+    // Reduce la salud del Pokémon basado en el daño recibido.
+    
+    public abstract void entrenar();
+    //Mejora las estadísticas del Pokémon.
+    
 }

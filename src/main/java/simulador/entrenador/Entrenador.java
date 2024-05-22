46c28b5b6287c1ce39b41ba7ad4adbbebd2bf8a2
Alexander Chacon <fresnedaalexander@gmail.com>
2024-05-22 01:02:30 -0500

    Desarrollo parte 1 clase entrenador

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -1,5 +1,42 @@
 package simulador.entrenador;
 
+import java.util.LinkedList;
+
 public class Entrenador {
-    // TODO: Aquí va tu código
+    private String nombre;
+    private LinkedList<Pokemon> listaPokemones;
+
+    public Entrenador(String nombre, LinkedList<Pokemon> listaPokemones) {
+        this.nombre = nombre;
+        this.listaPokemones = listaPokemones;
+    }
+    
+    public void agregarPokemon(Pokemon pokemon){
+        listaPokemones.add(pokemon);    
+    }
+    // Añade un Pokémon al equipo del entrenador.
+    
+    public void entrenarPokemon(Pokemon pokemon){
+    
+    }
+    //Selecciona un Pokémon para entrenar, mejorando sus estadísticas.
+    
+    public String mostrarPokemones(){
+        for (int i = 0; i < listaPokemones.size(); i++) {
+            return listaPokemones.get(i);
+        }
+        return null;
+    }
+    // Muestra una lista de los Pokémones actuales del entrenador.
+    
+    public Pokemon prepararBatalla(){
+        String palabraBusqueda=null;
+        for (int i = 0; i < listaPokemones.size(); i++) {
+            Pokemon nombrePokemonBusqueda=listaPokemones.get(i);
+            if(palabraBusqueda==nombrePokemonBusqueda.getNombre()){
+                return listaPokemones.get(i);
+            }
+        }
+    }
+    // Selecciona un Pokémon de su equipo para participar en una batalla.
 }
